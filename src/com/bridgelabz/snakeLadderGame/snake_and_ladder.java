46316ddccdd2e769705c44a92a80feaeaa74965a
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-11-03 20:38:57 +0530

    ADDED:Player rolls the die to get random number between 1 to 0

--- src/com/bridgelabz/snakeLadderGame/snake_and_ladder.java
+++ src/com/bridgelabz/snakeLadderGame/snake_and_ladder.java
@@ -4,8 +4,16 @@ public class snake_and_ladder {
     public static void main(String[] args) {
         //Taking position variable for starting position and assign it zero
         int position = 0;
-
         System.out.println("WELCOME TO SNAKE AND LADDER PROGRAMME");
         System.out.println("You are at Starting Position::" + position);
+        //calling a function Dice
+        Dice();
+    }
+
+    public static void Dice(){
+
+        //taking dice number by using random
+        int diceCheck = (int) Math.floor(Math.random() * 10) % 6+1;
+        System.out.println("Dice Value::"+ diceCheck);
     }
 }
