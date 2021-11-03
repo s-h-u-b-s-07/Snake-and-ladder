a3801fba840e11811088ed40c17f2e635656fccd
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-11-03 20:29:38 +0530

    ADDED:starting the game at 0 position

--- src/com/bridgelabz/snakeLadderGame/snake_and_ladder.java
+++ src/com/bridgelabz/snakeLadderGame/snake_and_ladder.java
@@ -2,7 +2,10 @@ package com.bridgelabz.snakeLadderGame;
 
 public class snake_and_ladder {
     public static void main(String[] args) {
+        //Taking position variable for starting position and assign it zero
         int position = 0;
-        System.out.println("Position::" + position);
+
+        System.out.println("WELCOME TO SNAKE AND LADDER PROGRAMME");
+        System.out.println("You are at Starting Position::" + position);
     }
 }
