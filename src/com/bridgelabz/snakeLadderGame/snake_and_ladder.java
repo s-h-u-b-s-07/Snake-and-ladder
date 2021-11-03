d193ecea7a620b9e3c6c8205d75a55499bbf4911
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-11-03 22:35:16 +0530

    ADDED:Player checks for option - Snake,Ladder or No play

--- src/com/bridgelabz/snakeLadderGame/snake_and_ladder.java
+++ src/com/bridgelabz/snakeLadderGame/snake_and_ladder.java
@@ -4,16 +4,31 @@ public class snake_and_ladder {
     public static void main(String[] args) {
         //Taking position variable for starting position and assign it zero
         int position = 0;
-        System.out.println("WELCOME TO SNAKE AND LADDER PROGRAMME");
-        System.out.println("You are at Starting Position::" + position);
-        //calling a function Dice
-        Dice();
-    }
+        System.out.println("position is " + position);
 
-    public static void Dice(){
+        //Taking die variable for dice number by using random
+        int die = (int) (Math.random() * 10) % 6 + 1;
+        System.out.println("dice value is " + die);
 
-        //taking dice number by using random
-        int diceCheck = (int) Math.floor(Math.random() * 10) % 6+1;
-        System.out.println("Dice Value::"+ diceCheck);
+        //Taking value variable by using random to compare with cases
+        int value = (int) (Math.random() * 10) % 3 + 1;
+        System.out.println("case number is " + value);
+
+        //computation
+        switch (value) {
+            case 1:
+                System.out.println("No Play You are in the same position.");
+                break;
+
+            case 2:
+                position = (position + die);
+                System.out.println("your position after ladder is ::" + position);
+                break;
+
+            case 3:
+                position = (position - die);
+                System.out.println("Your Position after snake is::" + position);
+                break;
+        }
     }
 }
