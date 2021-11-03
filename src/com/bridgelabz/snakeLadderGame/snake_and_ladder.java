363768177e6809a3b64988f01cb9a91d9261f9e9
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-11-03 22:55:22 +0530

    ADDED: Repeating till the player reaches 100th position

--- src/com/bridgelabz/snakeLadderGame/snake_and_ladder.java
+++ src/com/bridgelabz/snakeLadderGame/snake_and_ladder.java
@@ -6,29 +6,45 @@ public class snake_and_ladder {
         int position = 0;
         System.out.println("position is " + position);
 
-        //Taking die variable for dice number by using random
-        int die = (int) (Math.random() * 10) % 6 + 1;
-        System.out.println("dice value is " + die);
+        //Taking dicevalue variable for dice number by using random
+        int diceValue = (int) (Math.random() * 10) % 6 + 1;
 
-        //Taking value variable by using random to compare with cases
-        int value = (int) (Math.random() * 10) % 3 + 1;
-        System.out.println("case number is " + value);
+        //calling the class Utility
+        Utility utility = new Utility();
+        utility.playOption(position, diceValue);
+
+    }
+}
+
+class Utility {
+
+    public void playOption(int position, int diceValue) {
 
         //computation
-        switch (value) {
-            case 1:
-                System.out.println("No Play You are in the same position.");
-                break;
+        while (position <= 99) {
 
-            case 2:
-                position = (position + die);
-                System.out.println("your position after ladder is ::" + position);
-                break;
+            int optionValue = (int) (Math.random() * 10) % 3 + 1;
 
-            case 3:
-                position = (position - die);
-                System.out.println("Your Position after snake is::" + position);
-                break;
+            switch (optionValue) {
+                case 1:
+                    System.out.println("No Play....You are in the same position");
+                    break;
+
+                case 2:
+                    position = (position + diceValue);
+
+                    System.out.println("your position after ladder is :" + position);
+                    break;
+
+                case 3:
+                    position = (position - diceValue);
+
+                    if (position < 0) {
+                        position = 0;
+                    }
+                    System.out.println("Your Position after snake is :" + position);
+                    break;
+            }
         }
     }
 }
