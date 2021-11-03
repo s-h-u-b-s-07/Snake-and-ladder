e085c4289a55dda5b53aec7437ae7870e750f14c
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-11-03 19:55:45 +0530

    welcome to snake and ladder simulation

---
+++ src/com/bridgelabz/snakeLadderGame/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.snakeLadderGame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("SNAKE AND LADDER SIMULATOR GAME");
+    }
+}
