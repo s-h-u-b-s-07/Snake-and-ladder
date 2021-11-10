c877281a9a45bcd73dd7178a57846d07fc3eda79
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-11-11 00:17:20 +0530

    ADDED: 2nd Player in the game

--- src/com/bridgelabz/snakeLadderGame/snake_and_ladder.java
+++ src/com/bridgelabz/snakeLadderGame/snake_and_ladder.java
@@ -2,58 +2,90 @@ package com.bridgelabz.snakeLadderGame;
 
 public class snake_and_ladder {
     public static void main(String[] args) {
-        //Taking position variable for starting position and assign it zero
-        int position = 0;
+        //Taking positionPlayer variable for player1 and player2 for starting position and assign it zero
+        int positionPlayer1 = 0;
+        int positionPlayer2 = 0;
 
         //Taking diceRollCount variable for number of dice play
         int diceRollCount = 0;
 
-        System.out.println("position is " + position);
+        System.out.println("Player 1 position is ::" + positionPlayer1);
+        System.out.println("Player 2 position is ::" + positionPlayer2);
 
-        //Taking diceValue variable for dice number by using random
-        int diceValue = (int) (Math.random() * 10) % 6 + 1;
+        //Taking diceValPlayer variable for player1 and player2 for dice number by using random
+        int diceValPlayer1 = (int) (Math.random() * 10) % 6 + 1;
+        int diceValPlayer2 = (int) (Math.random() * 10) % 6 + 1;
 
         //calling the class Utility
         Utility utility = new Utility();
-        utility.playOption(position, diceValue, diceRollCount);
+        utility.playOption(positionPlayer1, positionPlayer2, diceValPlayer1, diceValPlayer2, diceRollCount);
 
     }
 }
 
 class Utility {
-
-    public void playOption(int position, int diceValue, int diceRollCount) {
+    public void playOption(int positionPlayer1, int positionPlayer2, int diceValPlayer1, int diceValPlayer2, int diceRollCount) {
 
         //computation
-        while (position <= 99) {
+        while ((positionPlayer1 <= 99) && (positionPlayer2 <= 99)) {
+            int optionValPlayer1 = (int) (Math.random() * 10) % 3 + 1;
+            int optionValPlayer2 = (int) (Math.random() * 10) % 3 + 1;
+            positionPlayer1 = (positionPlayer1 + diceValPlayer1);
+            positionPlayer2 = (positionPlayer2 + diceValPlayer2);
 
-            int optionValue = (int) (Math.random() * 10) % 3 + 1;
             diceRollCount++;
 
-            switch (optionValue) {
+            switch (optionValPlayer1) {
                 case 1:
                     System.out.println("No Play....You are in the same position");
                     break;
 
                 case 2:
-                    position = (position + diceValue);
-                    if (position > 100) {
-                        position = (position - diceValue);
+                    positionPlayer1 = (positionPlayer1 + diceValPlayer1);
+                    if (positionPlayer1 > 100) {
+                        positionPlayer1 = (positionPlayer1 - diceValPlayer1);
                         continue;
                     }
-                    System.out.println("your position after ladder is :" + position);
+                    System.out.println("Player 1 position after ladder is :" + positionPlayer1);
                     break;
 
                 case 3:
-                    position = (position - diceValue);
+                    positionPlayer1 = (positionPlayer1 - diceValPlayer1);
 
-                    if (position < 0) {
-                        position = 0;
+                    if (positionPlayer1 < 0) {
+                        positionPlayer1 = 0;
                     }
-                    System.out.println("Your Position after snake is" + position);
+                    System.out.println("Player 1 Position after snake is" + positionPlayer1);
                     break;
             }
+
+            switch (optionValPlayer2) {
+                case 1:
+                    System.out.println("No Play....You are in the same position");
+                    break;
+
+                case 2:
+                    positionPlayer2 = (positionPlayer2 + diceValPlayer2);
+                    if (positionPlayer2 > 100) {
+                        positionPlayer2 = (positionPlayer2 - diceValPlayer2);
+                        continue;
+                    }
+                    System.out.println("Player 2 position after ladder is :" + positionPlayer2);
+                    break;
+
+                case 3:
+                    positionPlayer1 = (positionPlayer2 - diceValPlayer2);
+
+                    if (positionPlayer2 < 0) {
+                        positionPlayer2 = 0;
+                    }
+                    System.out.println("Player 2 Position after snake is" + positionPlayer2);
+                    break;
+            }
+
         }
+        System.out.println("Player 1 final Position is: " + positionPlayer1);
+        System.out.println("Player 2 final Position is: " + positionPlayer2);
         System.out.println("dice play number:: " + diceRollCount);
     }
 }
